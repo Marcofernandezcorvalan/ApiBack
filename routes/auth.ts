96bdb015aa10df0bb35a22e4339f5d9ebbad829f
Marcofernandezcorvalan <marcofernandezc@outlook.com>
2023-09-11 16:14:02 -0300

    arreglo, check email

--- routes/auth.ts
+++ routes/auth.ts
@@ -10,7 +10,7 @@ router.post(
 	"/register",
 	[
 		check("name", "El nombre es obligatorio").not().isEmpty(),
-		check("email", "El email es obligatorio").isEmail,
+		check("email", "El email es obligatorio").isEmail(),
 		check("password", "La contraseña debe ser de 7 caracteres mínimo").isLength({ min: 7 }).not().isEmpty(),
 		check("email").custom(existsEmail),
 		catchErrors,
